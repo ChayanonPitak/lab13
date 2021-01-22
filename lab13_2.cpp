e1c30f715cd3d0eac3bc2a0e4d2b261e26330ad1
IkaWaAyuMu <pungpond4450@gmail.com>
2021-01-22 15:52:40 +0700

    lab13_2 Modified.

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,30 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool matrix[][N])
+{
+    for (int i=0 ; i<N ; i++)
+    {
+        for (int j=0 ; j<N ; j++) cout << matrix[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void inputMatrix(double matrix[][N])
+{
+    for (int i=0 ; i<N ; i++)
+    {
+        cout << "Row " << i+1 << ": ";
+		for (int j=0 ; j<N ; j++) cin >> matrix[i][j];
+	}
+}
+
+void findLocalMax(const double sourceMatrix[][N], bool destMatrix[][N])
+{
+    for (int i=0 ; i<N ; i++) for (int j=0 ; j<N ; j++) destMatrix[i][j] = false;
+	for (int i=1 ; i<N-1 ; i++) for (int j=1 ; j<N-1 ; j++)
+	{
+		if (sourceMatrix[i][j] >= sourceMatrix[i-1][j] && sourceMatrix[i][j] >= sourceMatrix[i+1][j] && sourceMatrix[i][j] >= sourceMatrix[i][j-1] && sourceMatrix[i][j] >= sourceMatrix[i][j+1]) destMatrix[i][j] = true;
+	}
+}
